dce0d18220fd496e45e86a2b1f61c8cd3bbc8c26
vaibhavahuja <vaibhavahuja@outlook.com>
2017-05-24 12:49:52 +0530

    2
    no change. different approach.

--- whatsapp automation.py
+++ whatsapp automation.py
@@ -1,26 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
-
 
 driver = webdriver.Chrome('C:/Users/vaibh/Desktop/vaibhavahuja/projects/chromedriver.exe')
+driver.get("http://web.whatsapp.com")
+try:
+    element = WebDriverWait(driver,20).until(
+        EC.presence_of_element_located((By.CLASS_NAME,"input"))
+    )
+finally:
+    name = driver.find_element_by_xpath('//*[@id="side"]/div[2]/div/label/input')
+    name.send_keys("Personal"+Keys.ENTER)
 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 600)
-target = "the name of contact or group "
+try:
+    element = WebDriverWait(driver,20).until(
+        EC.presence_of_element_located((By.XPATH,'//div[@class="input"][@dir="auto"][@data-tab="1"]'))
+    )
+finally:
+    message = driver.find_element_by_xpath('//div[@class="input"][@dir="auto"][@data-tab="1"]')
+    for i in range(10):
+        message.send_keys("hello" + Keys.ENTER)
 
-string = "add your message here"
-y_arg = '//*[@id="side"]/div[2]/div/label/input'
-input_y = wait.until(EC.presence_of_element_located((
-    By.XPATH, y_arg)))
-input_y.send_keys(target + Keys.ENTER)
-
-inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
-for i in range(70):
-    input_box.send_keys(string + Keys.ENTER)
-    time.sleep(0.04)
